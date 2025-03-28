f43dba7027f9e34fda1da06d4cc4730b6e6b268f
Ender <tomasz.b.mlynarczyk@gmail.com>
2025-03-28 23:14:01 +0100

    Dodałęm zegar i podstawę naliczania
    Dodałem grafikę zegara do sekcji zegara, będzie ona do wymiany później
    Zegar powinien być klikalny w całej niebieskiej area
    Powinny naliczać się sekundy i konwertować się na minuty, nic ponad to się nie konwertuje

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 // Variable containing upgrades area element
-const upgradeArea = document.getElementById("upgradeArea")
+const upgradeArea = document.getElementById("upgradeArea");
 // Function of the upgrade show button
 // used to show upgrade area on screens 
 // with screen width of less than 800px 
 function toggleUpgradeArea(){
-    upgradeArea.classList.toggle('show')
+    upgradeArea.classList.toggle('show');
+}
+
+let secondsCounter = 0
+let minutesCounter = 0
+let hoursCounter = 0
+let daysCounter = 0
+let monthsCounter = 0
+let yearsCounter = 0
+function addSecond(){
+    secondsCounter++;
+    if(secondsCounter >= 60){
+        secondsCounter = 0;
+        addMinute()
+    }
+    secondsAmount.innerHTML = secondsCounter+"";
+}
+
+function addMinute(){
+    minutesCounter++;
+    minutesAmount.innerHTML = minutesCounter+"";
 }
