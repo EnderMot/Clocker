e7451e786f00b123d52b9fbccfb08af5c1fc4552
Kamiko0o <klaudiarowickakr@gmail.com>
2025-04-16 01:26:23 +0200

    obiekt ulepszenia
    Obiekt ulepszenia 1 stworzony. Jedno kliknięcie i po nim robi się szary.

--- script.js
+++ script.js
@@ -73,3 +73,16 @@ function openSettings() {
   function closeSettings() {
     document.getElementById('settingsPanel').classList.add('hidden');
   }
+
+// Ulepszenia
+function activateUpgrade(element) {
+
+    if (element.classList.contains("used")) return;
+  
+    console.log("Ulepszenie aktywowane!");
+  
+    element.classList.add("used");
+  
+    // Tu możesz dodać dowolną logikę np. zwiększenie mnożnika itd.
+  }
+  
