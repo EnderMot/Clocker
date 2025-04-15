55e0debd793156bab984c41a808403ea5a8c07af
Kamiko0o <klaudiarowickakr@gmail.com>
2025-04-15 23:42:12 +0200

    nakladka
    naknjkasnkd
    
    Co-Authored-By: DL1S13CK1 <158756244+DL1S13CK1@users.noreply.github.com>

---
+++ nakładka_ustawienia/script.js
@@ -0,0 +1,8 @@
+function openSettings() {
+    document.getElementById('settingsPanel').classList.remove('hidden');
+  }
+  
+  function closeSettings() {
+    document.getElementById('settingsPanel').classList.add('hidden');
+  }
+  
